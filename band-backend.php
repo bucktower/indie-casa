90fa9a5a3c9118ded7a7f611a23918889b324e83
Buck Tower <bucktower14@gmail.com>
2015-07-14 17:19:45 -0500

    reggae should be complete for biography

--- band-backend.php
+++ band-backend.php
@@ -1,5 +1,10 @@
 <?php
 
+  // Include the wiki.php library -- this is used to parse Wiki Markup -> HTML
+  // Thank you https://github.com/lahdekorpi/Wiky.php
+  require_once("wiky.inc.php");
+
+
   $band_name = $_GET["band"];
   $prepped_band_name = str_replace(' ', '_', $band_name);
 
@@ -24,6 +29,28 @@
 
   $wiki_band_name = $band_info["query"]["pages"][$band_key[0]]["title"];
 
+  // get the actual wiki markup from the first part of the page
   $wiki_content = $band_info["query"]["pages"][$band_key[0]]["revisions"][0]["*"];
+
+  // for this variable, we only want the band's biography, none of that infobox-y
+  // stuff. this will only get text after the bold band name
   $wiki_content_p = strstr($wiki_content, "'''$wiki_band_name'''");
-?>
+
+  // Some preliminary parsing
+  // preg_replace & str_replace to get rid of those internal-Wikipedia links
+  $startPoint = "[[";
+  $endPoint = "|";
+  $wiki_content_p = str_replace("|", "", preg_replace('#('.preg_quote($startPoint).')([\w|\s|(|)|,]*)('.preg_quote($endPoint).')#si', '$1$3', $wiki_content_p));
+  $wiki_content_p = str_replace("[[", "", $wiki_content_p);
+  $wiki_content_p = str_replace("]]", "", $wiki_content_p);
+  $wiki_content_p = preg_replace("~\<ref\>(.*?)\</ref\>~", "", $wiki_content_p);
+
+  // now we must translate from wiki markup -> HTML
+  $wiky = new wiky;
+  $input=htmlspecialchars($wiki_content_p);
+  $wiki_content_p_parsed = $wiky->parse($input)
+
+  // Getting band's hometown/origin
+  // foundOrigin -- boolean (whether was able to find origin from Wikipedia)
+  //$foundOrigin = preg_match()
+  ?>
